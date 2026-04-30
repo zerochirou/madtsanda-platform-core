57f5a114ff32c171780ddb43fecd1c9d9cf53bc7
Zerochirou <226718015+zerochirou@users.noreply.github.com>
2026-04-30 13:59:56 +0700

    feat: integration with rustfs

--- start/env.ts
+++ start/env.ts
@@ -24,4 +24,20 @@ export default await Env.create(new URL('../', import.meta.url), {
 
   // Session
   SESSION_DRIVER: Env.schema.enum(['cookie', 'memory', 'database'] as const),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring the drive package
+  |----------------------------------------------------------
+  */
+  DRIVE_DISK: Env.schema.enum(['s3'] as const),
+  // AWS_ACCESS_KEY_ID: Env.schema.string(),
+  // AWS_SECRET_ACCESS_KEY: Env.schema.string(),
+  // AWS_REGION: Env.schema.string(),
+  // S3_BUCKET: Env.schema.string(),
+  S3_KEY: Env.schema.string(),
+  S3_SECRET: Env.schema.string(),
+  S3_BUCKET: Env.schema.string(),
+  S3_REGION: Env.schema.string(),
+  S3_ENDPOINT: Env.schema.string(),
 })
